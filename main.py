9d647c8c8a53cf74e52e6c89abeace9f90b707ba
enislalmi <enis_lalmi@hotmail.com>
2022-05-09 20:30:29 +0200

    tweets cleaning

---
+++ main.py
@@ -0,0 +1,203 @@
+import pandas as pd
+import numpy as np
+import matplotlib as nlp
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import warnings
+warnings.filterwarnings('ignore')
+import re
+import string
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import RegexpTokenizer
+from sklearn.model_selection import train_test_split
+
+tweets = pd.read_csv('tweets.csv')
+tweets['sentiment'] = tweets['sentiment'].replace(['negative'],'0')
+tweets['sentiment'] = tweets['sentiment'].replace(['positive'],'1')
+tweets['sentiment'] = tweets['sentiment'].replace(['neutral'],'2')
+print(tweets.head())
+#Stop Words: A stop word is a commonly used word (such as “the”, “a”, “an”, “in”) 
+#that a search engine has been programmed to ignore,
+#both when indexing entries for searching and when retrieving them as the result of a search query.
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+stopword = set(stopwords.words('english'))
+
+
+
+
+def clean_tweets(tweet):
+  # Lower Casing
+    tweet = re.sub(r"he's", "he is", tweet)
+    tweet = re.sub(r"there's", "there is", tweet)
+    tweet = re.sub(r"We're", "We are", tweet)
+    tweet = re.sub(r"That's", "That is", tweet)
+    tweet = re.sub(r"won't", "will not", tweet)
+    tweet = re.sub(r"they're", "they are", tweet)
+    tweet = re.sub(r"Can't", "Cannot", tweet)
+    tweet = re.sub(r"wasn't", "was not", tweet)
+    tweet = re.sub(r"don\x89Ûªt", "do not", tweet)
+    tweet = re.sub(r"aren't", "are not", tweet)
+    tweet = re.sub(r"isn't", "is not", tweet)
+    tweet = re.sub(r"What's", "What is", tweet)
+    tweet = re.sub(r"haven't", "have not", tweet)
+    tweet = re.sub(r"hasn't", "has not", tweet)
+    tweet = re.sub(r"There's", "There is", tweet)
+    tweet = re.sub(r"He's", "He is", tweet)
+    tweet = re.sub(r"It's", "It is", tweet)
+    tweet = re.sub(r"You're", "You are", tweet)
+    tweet = re.sub(r"I'M", "I am", tweet)
+    tweet = re.sub(r"shouldn't", "should not", tweet)
+    tweet = re.sub(r"wouldn't", "would not", tweet)
+    tweet = re.sub(r"i'm", "I am", tweet)
+    tweet = re.sub(r"I\x89Ûªm", "I am", tweet)
+    tweet = re.sub(r"I'm", "I am", tweet)
+    tweet = re.sub(r"Isn't", "is not", tweet)
+    tweet = re.sub(r"Here's", "Here is", tweet)
+    tweet = re.sub(r"you've", "you have", tweet)
+    tweet = re.sub(r"you\x89Ûªve", "you have", tweet)
+    tweet = re.sub(r"we're", "we are", tweet)
+    tweet = re.sub(r"what's", "what is", tweet)
+    tweet = re.sub(r"couldn't", "could not", tweet)
+    tweet = re.sub(r"we've", "we have", tweet)
+    tweet = re.sub(r"it\x89Ûªs", "it is", tweet)
+    tweet = re.sub(r"doesn\x89Ûªt", "does not", tweet)
+    tweet = re.sub(r"It\x89Ûªs", "It is", tweet)
+    tweet = re.sub(r"Here\x89Ûªs", "Here is", tweet)
+    tweet = re.sub(r"who's", "who is", tweet)
+    tweet = re.sub(r"I\x89Ûªve", "I have", tweet)
+    tweet = re.sub(r"y'all", "you all", tweet)
+    tweet = re.sub(r"can\x89Ûªt", "cannot", tweet)
+    tweet = re.sub(r"would've", "would have", tweet)
+    tweet = re.sub(r"it'll", "it will", tweet)
+    tweet = re.sub(r"we'll", "we will", tweet)
+    tweet = re.sub(r"wouldn\x89Ûªt", "would not", tweet)
+    tweet = re.sub(r"We've", "We have", tweet)
+    tweet = re.sub(r"he'll", "he will", tweet)
+    tweet = re.sub(r"Y'all", "You all", tweet)
+    tweet = re.sub(r"Weren't", "Were not", tweet)
+    tweet = re.sub(r"Didn't", "Did not", tweet)
+    tweet = re.sub(r"they'll", "they will", tweet)
+    tweet = re.sub(r"they'd", "they would", tweet)
+    tweet = re.sub(r"DON'T", "DO NOT", tweet)
+    tweet = re.sub(r"That\x89Ûªs", "That is", tweet)
+    tweet = re.sub(r"they've", "they have", tweet)
+    tweet = re.sub(r"i'd", "I would", tweet)
+    tweet = re.sub(r"should've", "should have", tweet)
+    tweet = re.sub(r"You\x89Ûªre", "You are", tweet)
+    tweet = re.sub(r"where's", "where is", tweet)
+    tweet = re.sub(r"Don\x89Ûªt", "Do not", tweet)
+    tweet = re.sub(r"we'd", "we would", tweet)
+    tweet = re.sub(r"i'll", "I will", tweet)
+    tweet = re.sub(r"weren't", "were not", tweet)
+    tweet = re.sub(r"They're", "They are", tweet)
+    tweet = re.sub(r"Can\x89Ûªt", "Cannot", tweet)
+    tweet = re.sub(r"you\x89Ûªll", "you will", tweet)
+    tweet = re.sub(r"I\x89Ûªd", "I would", tweet)
+    tweet = re.sub(r"let's", "let us", tweet)
+    tweet = re.sub(r"it's", "it is", tweet)
+    tweet = re.sub(r"can't", "cannot", tweet)
+    tweet = re.sub(r"don't", "do not", tweet)
+    tweet = re.sub(r"you're", "you are", tweet)
+    tweet = re.sub(r"i've", "I have", tweet)
+    tweet = re.sub(r"that's", "that is", tweet)
+    tweet = re.sub(r"i'll", "I will", tweet)
+    tweet = re.sub(r"doesn't", "does not", tweet)
+    tweet = re.sub(r"i'd", "I would", tweet)
+    tweet = re.sub(r"didn't", "did not", tweet)
+    tweet = re.sub(r"ain't", "am not", tweet)
+    tweet = re.sub(r"you'll", "you will", tweet)
+    tweet = re.sub(r"I've", "I have", tweet)
+    tweet = re.sub(r"Don't", "do not", tweet)
+    tweet = re.sub(r"I'll", "I will", tweet)
+    tweet = re.sub(r"I'd", "I would", tweet)
+    tweet = re.sub(r"Let's", "Let us", tweet)
+    tweet = re.sub(r"you'd", "You would", tweet)
+    tweet = re.sub(r"It's", "It is", tweet)
+    tweet = re.sub(r"Ain't", "am not", tweet)
+    tweet = re.sub(r"Haven't", "Have not", tweet)
+    tweet = re.sub(r"Could've", "Could have", tweet)
+    tweet = re.sub(r"youve", "you have", tweet)  
+    tweet = re.sub(r"donå«t", "do not", tweet)  
+    
+    tweet = re.sub(r"some1", "someone", tweet)
+    tweet = re.sub(r"yrs", "years", tweet)
+    tweet = re.sub(r"hrs", "hours", tweet)
+    tweet = re.sub(r"2morow|2moro", "tomorrow", tweet)
+    tweet = re.sub(r"2day", "today", tweet)
+    tweet = re.sub(r"4got|4gotten", "forget", tweet)
+    tweet = re.sub(r"b-day|bday", "b-day", tweet)
+    tweet = re.sub(r"mother's", "mother", tweet)
+    tweet = re.sub(r"mom's", "mom", tweet)
+    tweet = re.sub(r"dad's", "dad", tweet)
+    tweet = re.sub(r"hahah|hahaha|hahahaha", "haha", tweet)
+    tweet = re.sub(r"lmao|lolz|rofl", "lol", tweet)
+    tweet = re.sub(r"thanx|thnx", "thanks", tweet)
+    tweet = re.sub(r"goood", "good", tweet)
+    tweet = re.sub(r"some1", "someone", tweet)
+    tweet = re.sub(r"some1", "someone", tweet)
+    tweet = tweet.lower()
+    tweet=tweet[0:]
+    tweet = re.sub(r'((www\.[\S]+)|(https?://[\S]+))', ' URL ', tweet)
+    # Replace @handle with the word USER_MENTION
+    tweet = re.sub(r'@[\S]+', 'USER_MENTION', tweet)
+    # Replaces #hashtag with hashtag
+    tweet = re.sub(r'#(\S+)', r' \1 ', tweet)
+    # Remove RT (retweet)
+    tweet = re.sub(r'\brt\b', '', tweet)
+    # Replace 2+ dots with space
+    tweet = re.sub(r'\.{2,}', ' ', tweet)
+    # Strip space, " and ' from tweet
+    tweet = tweet.strip(' "\'')
+    # Replace emojis with either EMO_POS or EMO_NEG
+    tweet = handle_emojis(tweet)
+    # Replace multiple spaces with a single space
+    tweet = re.sub(r'\s+', ' ', tweet)
+    
+    #Remove punctuations
+    tweet = tweet.translate(str.maketrans("","",string.punctuation))
+    #tokenizing words
+    tokens = word_tokenize(tweet)
+    #tokens = [w for w in tokens if len(w)>2]
+    #Removing Stop Words
+    final_tokens = [w for w in tokens if w not in stopword]
+    #reducing a word to its word stem 
+    wordLemm = WordNetLemmatizer()
+    finalwords=[]
+    for w in final_tokens:
+      if len(w)>1:
+        word = wordLemm.lemmatize(w)
+        finalwords.append(word)
+    return ' '.join(finalwords)
+
+def handle_emojis(tweet):
+    # Smile -- :), : ), :-), (:, ( :, (-:, :')
+    tweet = re.sub(r'(:\s?\)|:-\)|\(\s?:|\(-:|:\'\))', ' EMO_POS ', tweet)
+    # Laugh -- :D, : D, :-D, xD, x-D, XD, X-D
+    tweet = re.sub(r'(:\s?D|:-D|x-?D|X-?D)', ' EMO_POS ', tweet)
+    # Love -- <3, :*
+    tweet = re.sub(r'(<3|:\*)', ' EMO_POS ', tweet)
+    # Wink -- ;-), ;), ;-D, ;D, (;,  (-;
+    tweet = re.sub(r'(;-?\)|;-?D|\(-?;)', ' EMO_POS ', tweet)
+    # Sad -- :-(, : (, :(, ):, )-:
+    tweet = re.sub(r'(:\s?\(|:-\(|\)\s?:|\)-:)', ' EMO_NEG ', tweet)
+    # Cry -- :,(, :'(, :"(
+    tweet = re.sub(r'(:,\(|:\'\(|:"\()', ' EMO_NEG ', tweet)
+    return tweet
+
+tweets['cleaned_tweets']  = tweets['text'].apply(lambda x: clean_tweets(x))  
+
+
+tokens=tweets['cleaned_tweets'].apply(lambda x: x.split())
+
+
+token = RegexpTokenizer(r'[a-zA-Z0-9]+')
+cv = CountVectorizer(stop_words='english',ngram_range = (1,1),tokenizer = token.tokenize)
+text_counts = cv.fit_transform(tweets['cleaned_tweets'].values.astype('U'))
+print(text_counts)
+
