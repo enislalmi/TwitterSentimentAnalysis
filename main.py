3505ed1030e1fb561e3e306522eb1fcbff669e24
enislalmi <enis_lalmi@hotmail.com>
2022-05-13 10:58:18 +0200

    adding plots

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from wordcloud import WordCloud, ImageColorGenerator, STOPWORDS
 from scipy.__config__ import show
 from PIL import Image
-
+import plotly.express as px
 import streamlit as st
 from utils import *
 import plotly.graph_objs as go
+from collections import Counter
 
 
 st.title("Twitter Sentiment Analysis")
@@ -81,16 +82,42 @@ data_neu=df[df['sentiment']=='2']['cleaned_tweets']
 pos_mask = np.array(Image.open('twitterimage.jpg'))
 
 fig1 =plot_wordcloud(data_pos,mask=pos_mask,color='white',max_font_size=100,title_size=30,title="Positive tweets")
-st.pyplot(fig1)
+#st.pyplot(fig1)
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 fig2 =plot_wordcloud(data_neg,mask=pos_mask,color='white',max_font_size=100,title_size=30,title="Negative tweets")
-st.pyplot(fig2)
+#st.pyplot(fig2)
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 fig3 =plot_wordcloud(data_neu,mask=pos_mask,color='white',max_font_size=100,title_size=30,title="Neutral tweets")
-st.pyplot(fig3)
+#st.pyplot(fig3)
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 
+df['temp_list'] = df['cleaned_tweets'].apply(lambda x:str(x).split())
+top = Counter([item for sublist in df['temp_list'] for item in sublist])
+temp = pd.DataFrame(top.most_common(20))
+temp.columns = ['Common_words','count']
+temp.style.background_gradient(cmap='Blues')
+
+fig3 = px.treemap(temp, path=['Common_words'], values='count',title='Most Common words through our dataset')
+st.plotly_chart(fig3)
+
+data_positive = df[df['sentiment']=='1']
+data_negative = df[df['sentiment']=='0']
+
+
+def most_common_words(data):
+    top = Counter([item for sublist in data['temp_list'] for item in sublist])
+    temp = pd.DataFrame(top.most_common(20))
+    temp.columns = ['Common_words','count']
+    temp.style.background_gradient(cmap='Greens')
+    fig4 = px.bar(temp, x="count", y="Common_words", title='Most Commmon Words', orientation='h', 
+                width=700, height=700,color='Common_words')
+    return fig4
+
+st.subheader("Our distribution of positive words:")
+st.plotly_chart(most_common_words(data_positive))
+st.subheader("Our distribution of negative words:")
+st.plotly_chart(most_common_words(data_negative))
